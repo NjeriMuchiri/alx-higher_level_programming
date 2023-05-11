3f7d552056009fec16806af6f2bf432d00b0e53c
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-11 14:51:04 +0300

    calculator operators

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,12 +7,11 @@ if __name__ == "__main__":
        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
        sys.exit(1)
     oper = sys.argv[2]
-    if oper != '+' and oper != '-' and '*' and oper != '/':
+    if oper != '+' and oper != '-' and oper != '*' and oper != '/':
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    
     if oper == '+':
         print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     elif oper == '-':
