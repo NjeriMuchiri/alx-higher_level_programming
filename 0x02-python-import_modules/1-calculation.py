d0f7465eb0657626ffb345d1c005bfe2b43021a5
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-11 13:16:02 +0300

    importing multiple functions

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -2,11 +2,7 @@
 from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
-print("{:d} + {:d} =  ".format(a, b), end='')
-print(add(a, b))
-print("{:d} + {:d} =  ".format(a, b), end='')
-print(sub(a, b))
-print("{:d} + {:d} =  ".format(a, b), end='')
-print(mul(a, b))
-print("{:d} + {:d} =  ".format(a, b), end='')
-print(div(a, b))
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} + {:d} = {:d}".format(a, b, mul(a,b)))
+print("{:d} + {:d} = {:d}".format(a, b, div(a, b)))
