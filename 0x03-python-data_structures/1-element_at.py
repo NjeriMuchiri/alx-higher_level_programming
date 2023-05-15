ac2aaceeb6b9f551d0f8cbe4d4ea3f13f516c8fa
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-15 10:09:07 +0300

    indexes

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0:
+    if idx > len(my_list) - 1:
         return (None)
-    elif idx == len(my_list) - 1:
+    elif idx < 0:
         return (None)
     return(my_list[idx])
