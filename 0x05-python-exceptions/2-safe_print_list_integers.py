0a4c5733bbd7dfa3596767cf64b232ca802acfbf
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-05-29 12:20:05 +0300

    index range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -4,11 +4,8 @@ def safe_print_list_integers(my_list=[], x=0):
     for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
-        except ValueError:
-            pass
-        except TypeError:
-            pass
-        else:
             count += 1
+        except (ValueError, TypeError):
+            pass
     print()
     return (count)
