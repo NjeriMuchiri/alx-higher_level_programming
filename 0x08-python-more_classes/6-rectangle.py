aa89e39f76c36dbab5e0c7152dabae343ed1e28d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-06-05 16:26:16 +0300

    number of instances of rectangle

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -8,6 +8,9 @@ class Rectangle:
     Attributes:
         number_of_instances (int): The number of Rectangle instances
     """
+
+    number_of_instances = 0
+
     def __init__(self, width=0, height=0):
         """Initializes a new Rectangle
 
