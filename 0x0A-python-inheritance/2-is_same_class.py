f738015da95f330f8aadff65a480176be7889586
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-06-12 15:33:12 +0300

    instance class

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+"""Function that checks the class type"""
+
+
 def is_same_class(obj, a_class):
     """ This function that returns True/False if obj is a type of a_class
 
     Args:
-        obj: object
+        obj(any): object to check
         a_class: type of the class
 
     Returns:
         True if type of obj is a_class
         False, otherwise
     """
-    return type(obj) is a_class
+    if type(obj) == a_class:
+        return True
+    return False
