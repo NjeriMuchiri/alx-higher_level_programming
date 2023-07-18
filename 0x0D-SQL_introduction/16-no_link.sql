41b0d0f51391d6778536f5cf833cfbb7fd91eb96
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-07-18 23:01:53 +0300

    converting to utf8

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- lists al records of the table second_table
 --lists all records of the table second_table who have the name value
-SELECT `score`, `name` FROM `second_table` WHERE `name` IS NOT NULL ORDER BY `score` DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
