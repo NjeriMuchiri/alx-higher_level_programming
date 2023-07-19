6d9f807c7ee1774f8f475f0f4748f437ec1cfeae
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-07-19 18:35:10 +0300

    sql privileges

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- Lists all privileges of MYSQL users user_0d_1 and user_0d_2 on the localhost server
+-- Query to list all privileges of MYSQL users
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
