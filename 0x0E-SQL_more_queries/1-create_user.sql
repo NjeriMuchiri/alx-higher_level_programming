55192db748fd8ef7fd2cd36b67f25ac175e30d8b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-07-19 18:45:54 +0300

    sql privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,10 +1,4 @@
 -- Creates the MYSQL server user user_0d_1 that should 
 -- have all privileges on the MYSQL server
-CREATE USER
-    IF NOT EXISTS 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES
-    ON *.* 
-    TO 'user_0d_1'@'localhost'
-    IDENTIFIED BY 'user_0d_1_pwd';
-FLUSH PRIVILEGES;
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
