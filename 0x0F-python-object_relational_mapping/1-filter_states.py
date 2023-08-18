ed34cd5406ae3e884516a80db138aa2daebe631f
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-18 18:14:39 +0300

    database states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import sys
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
-    curs.execute("SELECT * FROM `states` ORDER BY `id`")
+    curs.execute("SELECT * FROM states ORDER BY id;")
     states = curs.fetchall()
 
     for state in states:
