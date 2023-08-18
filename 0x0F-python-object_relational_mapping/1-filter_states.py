8e1c9954273dc1f87dca74d07fa0ebf86f20da7d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-18 16:08:16 +0300

    database states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     curs = db.cursor()
     curs.execute("SELECT * \
-    FROM states \
+    FROM `states` \
     ORDER BY `id`")
     states = curs.fetchall()
 
