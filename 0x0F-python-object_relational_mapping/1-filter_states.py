c002232ac6ee5e2c54f927ac5de1767480a7c428
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-18 18:11:54 +0300

    database states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,4 +8,3 @@ if __name__ == "__main__":
     curs = db.cursor()
     curs.execute("SELECT * FROM `states` ORDER BY `id`")
     [print(state) for state in curs.fetchall() if state[1][0] == "N"]
-    
