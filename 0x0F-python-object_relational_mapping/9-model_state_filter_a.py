6409b3e3bdc657cdcc03f3bf9e0db2dfac17d80b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-19 15:04:22 +0300

    orm usage

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,21 +1,19 @@
-#!/usr/bin/python3
-"""Prints the first state object from the database hbtn_0e_6_usa"""
+#!usr/bin/python3
+"""Lists all state objects that contain letter a from the database hbtn_0e_6_usa"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).order_by(State.id).first()
+    states = session.query(State).filter(State.name.like('%a%')).all()
 
-    if state is None:
-        print("Nothing")
-    else:
+    for state in states:
         print("{}: {}".format(state.id, state.name))
