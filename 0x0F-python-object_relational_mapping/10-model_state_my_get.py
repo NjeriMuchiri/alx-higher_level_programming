e7cebeb25b99864d60272b719000f3fbdaaaa21d
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-20 22:53:23 +0300

    model state get

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -14,9 +14,11 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
-
-    if state is None:
+    found = False
+    for state in session.query(State):
+        if state.name == sys.argv[4]:
+            print("{}".format(state.id))
+            found = True
+            break
+    if found is False:
         print("Not found")
-    else:
-        print("{}".format(state.id))
