0aa005a81828f53205325397c52207484ecb768b
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-08-20 21:10:20 +0300

    printing all states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
                          db=sys.argv[3], port=3306)
     curs = db.cursor()
-    curs.execute("SELECT * FROM `states` ORDER BY `id`;")
+    curs.execute("SELECT * FROM states ORDER BY id;")
     states = curs.fetchall()
 
     for state in states:
