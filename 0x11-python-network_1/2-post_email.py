2a4d4ac009e50380a3ace2d94e0399a37f62fdb5
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-01 12:10:22 +0300

    post an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Script that takes in a URL and an email
+sends a POST request to the passed URL with the email
+as a parameter and displays the body of the response"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == '__main__':
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values).encode('ascii')
+    with urllib.request.urlopen(sys.argv[1], data) as res:
+        html = res.read()
+        print(html.decode('utf-8'))
