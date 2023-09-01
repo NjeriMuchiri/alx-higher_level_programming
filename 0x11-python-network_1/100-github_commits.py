5d4f876e85fb84f3ddcb8f4c20fecd4813397fba
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-01 14:58:33 +0300

    github commits rails rails

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Script that list ten commits for a repository
+and an owner"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    the_repo = sys.argv[1]
+    the_owner = sys.argv[2]
+    the_url = "https://api.github.com/repos/{}/{}/commits".format(
+        the_repo, the_owner)
+
+    the_req = requests.get(the_url)
+    the_commits = the_req.json()
+    try:
+        for i in range(0, 10):
+            print("{}: {}".format(
+                the_commits[i].get("sha"),
+                the_commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
