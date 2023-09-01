8191f0773d844515858fef291aaaaf397e60dbc4
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-09-01 12:17:37 +0300

    post an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -8,12 +8,9 @@ import sys
 
 
 if __name__ == '__main__':
-    url = sys.argv[1]
-    value = {'email': sys.argv[2]}
+    value = {'Your email is': sys.argv[2]}
     data = urllib.parse.urlencode(value)
     data = data.encode('ascii')
-
-    request = urllib.request.Request(url, data)
-    with urllib.request.urlopen(request) as res:
+    with urllib.request.urlopen(sys.argv[1], data) as res:
         html = res.read()
         print(html.decode('utf-8'))
