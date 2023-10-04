baf39b9e28e7cf8a0e0b56da2d13a48d67743be9
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-10-04 12:54:39 +0300

    fetching data api

--- 0x15-javascript-web_jquery/7-script.js
+++ 0x15-javascript-web_jquery/7-script.js
@@ -1,4 +1,4 @@
 const $ = window.$;
-  $.get('https://swapi.co/api/people/5/?format=json', function (data) {
+  $.get('https://swapi-api.alx-tools.com/api/people/5/?format=json', function (data) {
     $('DIV#character').text(data.name);
   });
